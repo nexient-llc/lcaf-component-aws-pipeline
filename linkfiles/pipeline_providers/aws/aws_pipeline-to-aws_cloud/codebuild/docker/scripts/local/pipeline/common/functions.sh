97de6aade5647dd1f86756c1c87fd4121fe4b29c
Aaron <aaron-launch>
2023-12-11 23:47:54 +0000

    docker logic fixes

--- linkfiles/pipeline_providers/aws/aws_pipeline-to-aws_cloud/codebuild/docker/scripts/local/pipeline/common/functions.sh
+++ linkfiles/pipeline_providers/aws/aws_pipeline-to-aws_cloud/codebuild/docker/scripts/local/pipeline/common/functions.sh
@@ -11,7 +11,6 @@ function make_docker_build {
     local image_tag=$1
     local arch_type=$2
     export CONTAINER_IMAGE_VERSION="${image_tag}"
-    export DOCKER_BUILD_ARCH="${arch_type}"
 
     run_make_configure
     make platform/devenv/configure-docker-buildx
