3f8822a59963a24fe14959084979aa2108e42c0e
Aaron <aaron-launch>
2023-12-11 23:14:57 +0000

    docker logic fixes

--- linkfiles/pipeline_providers/aws/aws_pipeline-to-aws_cloud/codebuild/docker/scripts/local/pipeline/common/functions.sh
+++ linkfiles/pipeline_providers/aws/aws_pipeline-to-aws_cloud/codebuild/docker/scripts/local/pipeline/common/functions.sh
@@ -10,25 +10,25 @@ function run_conftest_docker {
 function make_docker_build {
     local image_tag=$1
     local arch_type=$2
+    export CONTAINER_IMAGE_VERSION="${image_tag}"
+    export DOCKER_BUILD_ARCH="${arch_type}"
 
     run_make_configure
     make platform/devenv/configure-docker-buildx
     echo "Container will be built with IMAGE_TAG=$image_tag"
-    export CONTAINER_IMAGE_VERSION="${image_tag}"
-    export DOCKER_BUILD_ARCH="${arch_type}"
     make docker/build
 }
 
 function make_docker_push {
     local image_tag=$1
     local arch_type=$2
+    export CONTAINER_IMAGE_VERSION="${image_tag}"
+    export DOCKER_BUILD_ARCH="${arch_type}"
 
     run_make_configure
     make platform/devenv/configure-docker-buildx
     make docker/aws_ecr_login
     echo "Container will be built with IMAGE_TAG=$image_tag"
-    export CONTAINER_IMAGE_VERSION="${image_tag}"
-    export DOCKER_BUILD_ARCH="${arch_type}"
     make docker/push
 }
 
